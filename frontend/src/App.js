28a0fa55b8b2eb25e7f2240fdc9e0aff9e5a5324
Karthik Raju <karthikraju391@gmail.com>
2022-04-26 20:32:46 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
+
+
+function App() {
+
+  return (
+    <div>
+      App
+    </div>
+  );
+}
+
+export default App;
