1a5a10802e5ffc38e9570fc34ddb5f8aa2bd1884
Yogesh-A-Gowda <67061755+Yogesh-A-Gowda@users.noreply.github.com>
2022-06-28 19:34:16 +0530

    Update cartRedux.js

--- client/src/redux/cartRedux.js
+++ client/src/redux/cartRedux.js
@@ -6,10 +6,8 @@ const cartSlice = createSlice({
     products: [],
     quantity: 0,
     total: 0,
-<<<<<<< HEAD
-=======
+
     wishlist: 0
->>>>>>> 1b641510357a7b0d0cd606f13c7fdc319185bbb7
   },
   reducers: {
     addProduct: (state, action) => {
@@ -17,12 +15,10 @@ const cartSlice = createSlice({
       state.products.push(action.payload);
       state.total += action.payload.price * action.payload.quantity;
     },
-<<<<<<< HEAD
   },
 });
 
 export const { addProduct } = cartSlice.actions;
-=======
     addToWishlist: (state) => {
       state.wishlist += 1;
     }
@@ -30,5 +26,4 @@ export const { addProduct } = cartSlice.actions;
 });
 
 export const { addProduct, addToWishlist } = cartSlice.actions;
->>>>>>> 1b641510357a7b0d0cd606f13c7fdc319185bbb7
 export default cartSlice.reducer;
